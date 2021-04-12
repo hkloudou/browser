ed7127773cf56fac862c07128d0db1f58a23d027
hkloudou <hkloudou@gmail.com>
2021-04-12 09:20:16 +0800

    auto commit

--- common.go
+++ common.go
@@ -2,32 +2,32 @@ package browser
 
 import "net/http"
 
-func (me *Browser) setAcceptAll(req *http.Request) {
+func (me *Browser) SetAcceptAll(req *http.Request) {
 	req.Header.Set("Accept", "*/*")
 }
 
-func (me *Browser) setAcceptHTML(req *http.Request) {
+func (me *Browser) SetAcceptHTML(req *http.Request) {
 	req.Header.Set("Accept", "text/html, application/xhtml+xml, image/jxr, */*")
 }
 
-func (me *Browser) setAcceptJSON(req *http.Request) {
+func (me *Browser) SetAcceptJSON(req *http.Request) {
 	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
 }
 
-func (me *Browser) setNoCache(req *http.Request) {
+func (me *Browser) SetNoCache(req *http.Request) {
 	req.Header.Set("Cache-Control", "no-cache")
 }
 
-func (me *Browser) setAcceptLang(req *http.Request) {
+func (me *Browser) SetAcceptLang(req *http.Request) {
 	req.Header.Set("Accept-Language", "zh-CN")
 }
 
 // X-Requested-With: XMLHttpRequest
-func (me *Browser) setAjax(req *http.Request) {
+func (me *Browser) SetAjax(req *http.Request) {
 	req.Header.Set("X-Requested-With", "XMLHttpRequest")
 }
 
-func (me *Browser) setReferer(req *http.Request, str string) {
+func (me *Browser) SetReferer(req *http.Request, str string) {
 	req.Header.Set("Referer", str)
 }
 func (me *Browser) SetHeader(req *http.Request, key, value string) {
@@ -36,11 +36,11 @@ func (me *Browser) SetHeader(req *http.Request, key, value string) {
 	req.Header.Set(key, value)
 }
 
-func (me *Browser) setContentJSON(req *http.Request) {
+func (me *Browser) SetContentJSON(req *http.Request) {
 	// req.Header.Set("Referer", str)
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 }
 
-func (me *Browser) setContentFormPost(req *http.Request) {
+func (me *Browser) SetContentFormPost(req *http.Request) {
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 }
